6370966992e730aa2a3231dff704d8e28f1a00cd
LiamSL <liamleung@outlook.com>
2021-01-12 16:38:58 -0500

    almost done menu

--- lab0/index.js
+++ lab0/index.js
@@ -17,7 +17,29 @@ function make_code(length){
 }
 
 function get_main_menu_selection(){
-    
+    console.log()
+    console.log("What do you want to do")
+    console.log("(i) Show Instructions")
+    console.log("(p) Play a game")
+    console.log("(q) Quit")
+
+    response = input("")
+
+    if (response == "i"){
+        print_instructions()
+    }
+
+    else if (response == "p"){
+        play_round()
+    }
+
+    else if (console.log (response == "q")){
+        console.log("Ok bye!")
+    }
+
+    else{
+        console.log("I don't understand...")
+    }
 }
 
 function init() {
