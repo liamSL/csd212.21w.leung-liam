a10d8cd238df37334aeefe255f3c6f0fd600c774
LiamSL <liamleung@outlook.com>
2021-01-12 15:17:47 -0500

    git commit csd212.hub

---
+++ lab0/index.js
@@ -0,0 +1,27 @@
+console.log("Hello, World!");
+
+function all_true(a, b){
+
+}
+
+function is_numeric(c){
+
+}
+
+function is_string_numeric(s){
+
+}
+
+function make_code(length){
+    
+}
+
+function get_main_menu_selection(){
+
+}
+
+function init() {
+    console.log("CODE⚡BREAKER")
+    get_main_menu_selection()
+}
+init()
