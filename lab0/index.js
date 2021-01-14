e63f8279242c5a6288bd709bb6b3a99bc04ecd9d
LiamSL <liamleung@outlook.com>
2021-01-14 13:43:06 -0500

    git commit prompt-sync

--- lab0/index.js
+++ lab0/index.js
@@ -1,5 +1,8 @@
 console.log("Hello, World!");
 
+import initPrompt from 'prompt-sync'
+const prompt = initPrompt();
+
 function all_true(a, b){
 
 }
@@ -16,6 +19,44 @@ function make_code(length){
     
 }
 
+function get_guess(code_length){
+
+}
+
+function count_matches(code, guess){
+
+}
+
+function get_code_length(){
+    response = prompt("How long do you want the code to be? ")
+
+    if (!(is_string_numeric(response)))
+}
+
+function play_round(){
+
+}
+
+function get_history_path(){
+
+}
+
+function write_history(){
+
+}
+
+function update_history(){
+
+}
+
+function load_history(){
+
+}
+
+function print_instructions(){
+    console.log("You select a code length.  The computer will pick a random numeric code of that length.  You then try to guess the code.  On every guess, the computer will tell you how many numbers in your guess are both the correct number and in the correct position (★) and how many are the correct number but not in the correct position (☆).  Using this information, you should eventually be able to deduce the correct code!")
+}
+
 function get_main_menu_selection(){
     console.log()
     console.log("What do you want to do")
@@ -23,14 +64,14 @@ function get_main_menu_selection(){
     console.log("(p) Play a game")
     console.log("(q) Quit")
 
-    let response = input("")
+    let response = prompt("");
 
     if (response == "i"){
-        print_instructions()
+        print_instructions();
     }
 
     else if (response == "p"){
-        play_round()
+        play_round();
     }
 
     else if (console.log (response == "q")){
@@ -45,6 +86,6 @@ function get_main_menu_selection(){
 
 function init() {
     console.log("CODE⚡BREAKER")
-    get_main_menu_selection()
+    get_main_menu_selection();
 }
 init()
