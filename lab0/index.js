ad2f2e10108ae35d368e18032236e3d480bec225
LiamSL <liamleung@outlook.com>
2021-01-29 09:44:34 -0500

    almost done

--- lab0/index.js
+++ lab0/index.js
@@ -5,12 +5,12 @@ console.log("Hello, World!");
 import initPrompt from 'prompt-sync';
 const prompt = initPrompt();
 
-import existsSync from "fs";
+import path from 'path';
 
-import {readFileSync, writeFileSync} from "fs";
+import {readFileSync, writeFileSync, existsSync, openSync} from "fs";
 
-import {homedir, EOL} from "os";
-import { exception } from 'console';
+import * as os from "os";
+import { pathToFileURL } from 'url';
 
 function all_true(a, b){
     return a && b;
@@ -29,53 +29,57 @@ function is_string_numeric(s){
 function make_code(length){
     let code = "";
     for (const _ of Array(length).keys()){
-        code += String(Math.floor(Math.random(0, 9)*10));}
+        code += Math.floor(Math.random()*10);}
+    return code;
 }
 
 function get_guess(code_length){
     let guess = prompt("Guess the code: ");
 
     if (guess.length != code_length){
-        console.log("You must enter ${code_length} numbers");}
+        console.log(`You must enter ${code_length} numbers`);
+        get_guess(code_length);}
 
     if (!is_string_numeric(guess)){
         console.log("The code may contain only numbers");
         return get_guess(code_length);}
+    return guess;
 }
 
 function count_matches(code, guess){
-    code_numbers = Array.from(code);
-    guess_numbers = Array.from(guess);
+    let code_numbers = Array.from(code);
+    let guess_numbers = Array.from(guess);
 
     let num_matches = 0;
     let num_semi_matches = 0;
 
     for(const i of Array(code_numbers.length).keys()){
-        if (code_numbers[i] == guess_numbers[i]){
+        if (code_numbers[i] === guess_numbers[i]){
             code_numbers[i] = "-";
             guess_numbers[i] = "-";
-            num_matches += 1;}
+            num_matches += 1;
+        }
     }
 
-    for(g in guess_numbers){
+    for(const g of guess_numbers){
         if (g === "-"){
             continue;}
-    }
 
-    for(const i of Array(code_numbers.length).keys()){
-        let c = code_numbers[i];
+        for(const i of Array(code_numbers.length).keys()){
+            let c = code_numbers[i];
 
-        if (c === "-"){
-            continue;
-        }
+            if (c === "-"){
+                continue;
+            }
         
-        if (g === c){
-            code_numbers[i] = "-";
-            num_semi_matches += 1;
-            break;
+            if (g === c){
+                code_numbers[i] = "-";
+                num_semi_matches += 1;
+                break;
+            }
         }
-    return {exactMatches: num_matches, semiMatches: num_semi_matches};
     }
+    return [num_matches, num_semi_matches];
 }
 
 function get_code_length(){
@@ -85,56 +89,60 @@ function get_code_length(){
         console.log("You must enter a number");
         return get_code_length();}
 
-    let n = Number(response);
+    let code_length = Number(response);
 
-    if (n < 2){
-        console.log("You must choose a number greater than 1");
+    if (code_length < 2){
+        console.log("You must choose a number greater than 1 ");
         return get_code_length();}
 
-    return n;
+    return code_length;
 }
 
 function play_round(){
     let code_length = get_code_length();
 
     let history = load_history();
-    if (code_length in history){
-        let [num_games, best, average] = history[code_length];
+    if (history.has(code_length)){
+        console.log(history.get(code_length));
+        let {num_games, best, average} = history.get(code_length);
         console.log(`The number of times you have tried codes of length ${code_length} is ${num_games}. Your average and best number of guesses are ${average} and ${best} respectively.`);
     }
     else{
         console.log(`This is your first time trying a code of length ${code_length}`);
     }
-    let code = Make_code(code_length);
+    let code = make_code(code_length);
     let num_guesses = 0;
 
     while(true){
         num_guesses += 1;
-        guess = get_guess(code_length);
-
-        if (guess === code){
+        let guess = get_guess(code_length);
+        switch(guess){
+        
+        case code:
             console.log(`You cracked the code! Number of guesses: ${num_guesses}`);
             update_history(history, code_length, num_guesses);
+
             return;
-        }
-        else{
-            [num_matches, num_semi_matches] = count_matches(code, guess);
-            console.log("★"*num_matches + "☆"*num_semi_matches + "-"*(code_length-num_matches-num_semi_matches));
+        
+        default:
+            let [num_matches, num_semi_matches] = count_matches(code, guess);
+            console.log("★".repeat(num_matches) + "☆".repeat(num_semi_matches) + "-".repeat(code_length-num_matches-num_semi_matches));
         }
     }
 }
 
 function get_history_path(){
-    return path.join(os.homedir(), "CODEBREAKER.history");
+    let history_path = path.join(os.homedir(), "CodeBreaker.txt");
+    return history_path;
 }
 
 function write_history(history){
-    history_path = get_history_path;
-    if (fs.existsSync(history_path)){
+    let history_path = get_history_path();
+    if (existsSync(history_path)){
         try{
             let hist = `${code_length}:${data[0]}:${data[1]}:${data[2]}` + os.EOL;
-            for ({code_length, data} in history){
-                fs.writeFileSync(history_path, hist);
+            for (const {key, value} of history.entries()) {
+                writeFileSync(history_path, hist);
             }
         }
         catch{
@@ -144,8 +152,8 @@ function write_history(history){
 }
 
 function update_history(history, code_length, num_guesses){
-    if (code_length in history){
-        (num_games, best, average) = history[code_length];
+    if (history.has(code_length)){
+        let {num_games, best, average} = history.get(code_length);
 
         if (num_guesses < best){
             console.log(`${num_guesses} is a new best score for codes of length ${code_length}!`);
@@ -154,29 +162,31 @@ function update_history(history, code_length, num_guesses){
         else if(num_guesses < average) {
             console.log(`${num_guesses} is better than your average score of ${average} for codes of length ${code_length}!`);
         }
-        let average = ((average*num_games) + num_games)/(num_games + 1);
+        average = ((average*num_games) + num_games)/(num_games + 1);
         num_games += 1;
         history[code_length] = (num_games, best, average);
     }
-        else {
-            history[code_length] = (1, num_guesses, num_guesses);
-        }
-        write_history(history);
+    else {
+        history[code_length] = (1, num_guesses, num_guesses);
+    }
+    write_history(history);
     
 }
 
 function load_history(){
-    history_path = get_history_path();
+    let history_path = get_history_path();
 
-    history = {};
+    let history = new Map();
 
-    if (fs.existsSync(history_path)){
+    if (existsSync(history_path)){
         try{
-        const content = fs.readFileSync(history_path).toString().split("\n");
-        for (const line in content){
-            (code_length, num_games, best, average) = line.split(":");
-            history[Number(code_length)] = (Number(num_games), Number(best), +(average));
+        let content = readFileSync(history_path).toString().split(os.EOL);
+        for (let line of content){
+            let [code_length, num_games, best, average] = line.split(":");
+            let dict = {"num_games": +(num_games), "best": +(best), "average": +(average)};
+            history.set(+(code_length), dict);
         }
+        console.log(history);
     }
         catch{
             console.log("Uh oh, your history file could not be read");
@@ -184,10 +194,10 @@ function load_history(){
     }
     else {
         try {
-            fs.writeFileSync(history_path, "");
+            writeFileSync(history_path, "");
         }
         catch {
-            console.log("Uh oh, I couldn't creat a history file for you");
+            console.log("Uh oh, I couldn't create a history file for you");
         }
     }
     return history
@@ -220,6 +230,7 @@ function get_main_menu_selection(){
         default:
             console.log("I don't understand...")
     }
+    get_main_menu_selection();
 }
 
 function init() {
