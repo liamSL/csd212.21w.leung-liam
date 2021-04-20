bcad75f369959adb332dec17fba2a7fc6a7e33b9
LiamSL <liamleung@outlook.com>
2021-04-20 14:16:02 -0400

    4 most done

--- lab5/app.js
+++ lab5/app.js
@@ -1,3 +1,35 @@
+function init(){
+    document.getElementById('ingredients').onchange = async () => {
+        let resp = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${document.getElementById('ingredients').selectedOptions[0].innerHTML}`, 
+        {
+            method: 'POST',
+            headers: {
+            'Content-Type' : 'application/x-www-form-urlencoded'
+            }
+        }
+        );
+        if (! resp.ok){
+            throw new Error("an error has occured, try reloading the page");
+        }
+        let data = await resp.json();
+        document.getElementById('results').innerHTML = "";
+        for (const sugg of data.drinks){
+            const result = document.createElement('div');
+            result.classList.add('result');
+            const img = document.createElement('img');
+            img.src = sugg.strDrinkThumb;
+            img.alt = sugg.idDrink;
+            img.classList.add('photo');
+            result.appendChild(img);
+            const desc = document.createElement('label');
+            desc.textContent = sugg.strDrink;
+            desc.classList.add('name');
+            result.appendChild(desc);
+            document.getElementById('results').appendChild(result);
+        }
+    }
+}
+
 addEventListener('load', getIngredients);
 
 async function getIngredients(){
@@ -14,6 +46,7 @@ async function getIngredients(){
             document.getElementById('ingredients').appendChild(sl);
         }
         document.getElementById('ingredients').selectedIndex = -1;
+        init();
     } catch(error) {
         console.log(error);
     }
